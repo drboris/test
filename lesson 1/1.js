40c0c577e725ced2b58ced8d2ce716b9767da11e
DrBoris <borisdoktorov@gmail.com>
2018-10-01 18:44:01 +0300

    First commit

---
+++ lesson 1/1.js
@@ -0,0 +1,27 @@
+
+
+
+
+var budget = prompt("Your buget for month?", "10000");
+var name = prompt("Name of your shop", "Provence");
+var shopGoods1 = prompt("Name your first item", "Beer");
+var shopGoods2 = prompt("Name your second item", "Wine");
+var shopGoods3 = prompt("Name your third item", "Vodka");
+var shopGoods = [shopGoods1,shopGoods2,shopGoods3];
+
+mainList = {
+	budget,
+	name,
+	shopGoods,
+	employers,
+}
+
+employers = {
+
+
+}
+
+prompt(budget/30);
+console.log(name);
+console.log(mainList);
+console.log(shopGoods);
